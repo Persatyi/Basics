53f382e423bb28f599cb782f7f8373033672862c
Володимир Іванченко <persatyi@ukr.net>
2026-04-24 13:59:23 +0300

    new exercise

--- pointers/practice_module/chapter4/exercise6.c
+++ pointers/practice_module/chapter4/exercise6.c
@@ -5,3 +5,55 @@
 Виведи всі елементи
 Звільни пам'ять
 */
+#include <stdlib.h>
+#include <stdio.h>
+
+void fillArr(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand();
+    }
+}
+
+int *createArray(int size){
+    int *tmp = (int *)malloc(size * sizeof *tmp);
+    if(tmp == NULL) {
+        return NULL;
+    }
+
+    fillArr(tmp, size);
+
+    return tmp;
+}
+
+void printArray( int *arr, int size) {
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void generateArray(int **arr, int size, int *sizes) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = createArray(sizes[i]);
+        printArray(arr[i], sizes[i]);
+    }
+}
+
+void freeMem(int **arr, int size){
+    for (int i = 0; i < size; i++) {
+        free(arr[i]);
+        arr[i] = NULL;
+    }
+}
+
+int main() {
+    int size = 3;
+    int *arr[size];
+    int sizes[] = {2, 4, 3};
+
+    generateArray(arr, size, sizes);
+    freeMem(arr, size);
+
+    return 0;
+}
