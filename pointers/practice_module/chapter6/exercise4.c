2b001c5346881e1c351c18d76b53b88c1c7f0ac5
Володимир Іванченко <persatyi@ukr.net>
2026-06-01 12:35:45 +0300

    practice

--- pointers/practice_module/chapter6/exercise4.c
+++ pointers/practice_module/chapter6/exercise4.c
@@ -32,3 +32,15 @@ void addHead(LinkedList *list, void *data) {
     }
     list->head = node;
 }
+
+void addTail(LinkedList *list, void *data) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    if(list->head == NULL) {
+        list->head = node;
+    } else {
+        list->tail->next = node;
+    }
+    list->tail = node;
+}
