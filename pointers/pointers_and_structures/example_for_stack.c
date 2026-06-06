57c14d0a94a1c4a1df7797ab180237f76e2ac3a0
Володимир Іванченко <persatyi@ukr.net>
2026-06-06 23:16:10 +0300

    update

--- pointers/pointers_and_structures/example_for_stack.c
+++ pointers/pointers_and_structures/example_for_stack.c
@@ -4,3 +4,21 @@
 void push(Stack *stack, void* data) {
  addHead(stack, data);
 }
+
+
+void *pop(Stack *stack) {
+ Node *node = stack->head;
+ if (node == NULL) {
+ return NULL;
+ } else if (node == stack->tail) {
+ stack->head = stack->tail = NULL;
+ void *data = node->data;
+ free(node);
+ return data;
+ } else {
+ stack->head = stack->head->next;
+ void *data = node->data;
+ free(node);
+ return data;
+ }
+}
